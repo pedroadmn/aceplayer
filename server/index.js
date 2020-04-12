0765a0c544f94169eca1aecec2c7a797e93507d2
Pedro Adelino de Mello Neto <pedroadmn@gmail.com>
2020-04-12 08:49:38 -0300

    Initializing Subscriber component

--- server/index.js
+++ server/index.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const app = express();
-const path = require("path");
-const cors = require('cors')
-
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
@@ -15,26 +12,18 @@ const config = require("./config/key");
 //   .catch(err => console.error(err));
 
 const mongoose = require("mongoose");
-const connect = mongoose.connect(config.mongoURI,
-  {
-    useNewUrlParser: true, useUnifiedTopology: true,
-    useCreateIndex: true, useFindAndModify: false
-  })
+const connect = mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-app.use(cors())
 
-//to not get any deprecation warning or error
-//support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
-//to get json data
-// support parsing of application/json type post data
 app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use('/api/users', require('./routes/users'));
 app.use('/api/video', require('./routes/video'));
+app.use('/api/subscribe', require('./routes/subscribe'));
 
 
 //use this to show the image you have in node js server to client (react js)
@@ -49,7 +38,7 @@ if (process.env.NODE_ENV === "production") {
 
   // index.html for all page routes
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
