044681611fda4fc165ba3c4731384d0cbbfcbc2c
Pedro Adelino de Mello Neto <pedroadmn@gmail.com>
2020-04-12 09:31:17 -0300

    Subscribe and Unsubscribe functions

--- client/src/components/views/DetailVideoPage/Sections/Subscriber.js
+++ client/src/components/views/DetailVideoPage/Sections/Subscriber.js
@@ -1,12 +1,47 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
-
 function Subscriber(props) {
     const userTo = props.userTo
     const userFrom = props.userFrom
 
     const [SubscribeNumber, setSubscribeNumber] = useState(0)
     const [Subscribed, setSubscribed] = useState(false)
+
+    const onSubscribe = ( ) => {
+
+        let subscribeVariables = {
+                userTo : userTo,
+                userFrom : userFrom
+        }
+
+        if(Subscribed) {
+            //when we are already subscribed 
+            axios.post('/api/subscribe/unSubscribe', subscribeVariables)
+                .then(response => {
+                    if(response.data.success){ 
+                        setSubscribeNumber(SubscribeNumber - 1)
+                        setSubscribed(!Subscribed)
+                    } else {
+                        alert('Failed to unsubscribe')
+                    }
+                })
+
+        } else {
+            // when we are not subscribed yet
+            
+            axios.post('/api/subscribe/subscribe', subscribeVariables)
+                .then(response => {
+                    if(response.data.success) {
+                        setSubscribeNumber(SubscribeNumber + 1)
+                        setSubscribed(!Subscribed)
+                    } else {
+                        alert('Failed to subscribe')
+                    }
+                })
+        }
+
+    }
+
 
     useEffect(() => {
 
@@ -34,13 +69,14 @@ function Subscriber(props) {
 
     return (
         <div>
-            <button style={{
-                backgroundColor: `${Subscribed ? '#AAAAA' : '#CC0000'}`,
+            <button 
+            onClick={onSubscribe}
+            style={{
+                backgroundColor: `${Subscribed ? '#AAAAAA' : '#CC0000'}`,
                 borderRadius: '4px', color: 'white',
                 padding: '10px 16px', fontWeight: '500', fontSize: '1rem', textTransform: 'uppercase'
             }}>
-                {SubscribeNumber} {Subscribed ? 'Subscribed' :
-                    'Subscribe'}
+                {SubscribeNumber} {Subscribed ? 'Subscribed' : 'Subscribe'}
             </button>
         </div>
     )
